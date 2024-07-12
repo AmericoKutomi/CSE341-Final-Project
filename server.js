cf306b2e1c13232e2dc5d7eb74db40f11be3b075
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2024-07-12 19:32:58 +0100

    update packages

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv')
+const port = process.env.PORT
+
+app.listen(port, () =>{
+    console.log(`Running in port ${port}`);
+})
