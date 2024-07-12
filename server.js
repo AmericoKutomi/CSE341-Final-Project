ee26906ad046be89a42cfd3db007f59e392bbc3c
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2024-07-12 21:43:23 +0100

    configuration of server

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const mongodb = require('./database/connect')
 const dotenv = require('dotenv')
+const bodyParser = require('body-parser')
 const port = process.env.PORT
 
-app.listen(port, () =>{
-    console.log(`Running in port ${port}`);
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use('/', require('./routes/index') )
+mongodb.initDb((err) => {
+    if(err){
+        console.log(err)
+    }else{
+        app.listen(port, () => {
+            console.log(`Running on port ${port}`)
+        })
+    }
 })
