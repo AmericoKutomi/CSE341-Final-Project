8d197a09f0ff1ed75c9f4c66054d09d53a14d633
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2024-07-13 23:02:06 +0100

    teacherController file in updated

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -1,30 +1,30 @@
-const mongodb = require('../database/connect')
+const db = require('../models/teacherModel')
 const ObjectId = require('mongodb').ObjectId
 
 const getAllTeacher = async (req, res) => {
-    const dataResult = await mongodb.getDb().db().collection('teachers').find().toArray().then((teachers) => {
+   //#swagger.tags=['Teachers']
+   const teachers = await db.getAll();
         res.setHeader('content-type', 'application/json')
         res.status(200).json(teachers)
-    })
+    
 }
 
 const getSingleTeacher = async (req, res) => {
-   
+    //#swagger.tags=['Teachers']
     if(!ObjectId.isValid(req.params.id)){
         res.status(400).json('Must use valid contact id to get that contact.')
             }
-    const teacher_id = new ObjectId(req.params.id);
+    
     try{
-        const result = await mongodb.getDb().db().collection('teachers').find({_id: teacher_id});
-        result.toArray().then((teacher)=> {
-            if (!teacher[0]){
-                return res.status(404).json({error: 'Contact not found'})
+        const teacher = await db.getSingle(req.params.id);
+            if (!teacher){
+                return res.status(404).json({error: 'Teacher not found'})
             }else{
                 res.setHeader('content-type', 'application/json');
-              return  res.status(200).json(teacher[0]);
+              return  res.status(200).json(teacher);
             }
        
-        });
+        
     }catch(error){
 console.log(error)
 return res.status(500).json({error: 'Internal Server'})
@@ -34,6 +34,7 @@ return res.status(500).json({error: 'Internal Server'})
 };
 
 const CreateTeacher = async (req, res) => {
+     //#swagger.tags=['Teachers']
     const teacher = {
         name: req.body.name,
         email: req.body.email,
@@ -42,8 +43,8 @@ const CreateTeacher = async (req, res) => {
         experience: req.body.experience,
         subjects_taught: req.body.subjects_taught
     }
-    const dataResult = await mongodb.getDb().db().collection('teachers').insertOne(teacher)
-    if(dataResult.acknowledged){
+    const dataResult = await db.createTeacher(teacher)
+    if(dataResult){
         res.status(201).send()
     }else{
         res.status(500).json(dataResult.error || 'Some error occured while create the teacher.')
@@ -51,11 +52,12 @@ const CreateTeacher = async (req, res) => {
 }
 
 const updateTeacher = async (req, res) => {
+     //#swagger.tags=['Teachers']
 
     if(!ObjectId.isValid(req.params.id)){
         res.status(400).json('Must use valid id teacher to update a teacher.')
     }
-    const teacher_id = new ObjectId(req.params.id)
+    
     const teacher = {
         name: req.body.name,
         email: req.body.email,
@@ -66,13 +68,13 @@ const updateTeacher = async (req, res) => {
        
     }
     
-    const dataResult = await mongodb.getDb().db().collection('teachers').replaceOne({_id: teacher_id}, teacher, {runValidators: true})
+    const dataResult = await db.updateTeacher(teacher, req.params.id)
     
     if(dataResult.modifiedCount === 0){
         res.status(404).json({status:'fail',
         msg: 'No teacher with ID ' +teacher_id+' is found to update.'})
     }
-    if(dataResult.modifiedCount > 0){
+    if(dataResult){
         res.status(204).json('teacher updated')
     }else{
         res.status(500).json(dataResult.error || 'Some error occured while update the teacher.')
@@ -80,22 +82,22 @@ const updateTeacher = async (req, res) => {
 }
 
 const deleteTeacher = async (req, res) => {
+     //#swagger.tags=['Teachers']
 
     if(!ObjectId.isValid(req.params.id)){
         res.status(400).json('Must use valid id teacher to delete a teacher.')
     }
 
-    const teacher_id = new ObjectId(req.params.id)
     
-    const dataResult = await mongodb.getDb().db().collection('teachers').deleteOne({_id: teacher_id}, true)
+    const dataResult = await db.deleteTeacher(req.params.id)
    
-    if(dataResult.deletedCount === 0 & dataResult.acknowledged === true){
+    if(dataResult === 0 ){
         return res.status(404).json({
             status:'fail',
             msg:'No teacher with ID ' +teacher_id+' is found to delete.'
         })
        
-    } else if(dataResult.deletedCount > 0 & dataResult.acknowledged === true){
+    } else if(dataResult > 0){
        return res.status(204).json({status:"success"})
     }
     else{
