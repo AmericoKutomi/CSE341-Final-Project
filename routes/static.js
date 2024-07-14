772d93219a52090a2f40e5a6abc7233befe5d28b
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2024-07-14 01:20:56 +0100

    new static file

---
+++ routes/static.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// Static Routes
+// Set up "public" folder / subfolders for static files
+router.use(express.static("public"));
+
+
+
+module.exports = router;
+
+
+
