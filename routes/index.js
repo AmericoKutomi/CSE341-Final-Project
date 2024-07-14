9b7f7e97b99b2e1b97ee098068372e14d69ff051
Jarom Mariscal <jaromero_marz234@outlook.com>
2024-07-13 18:13:19 -0700

    Student controller, student model and routes were created

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ router.get('/', require('./static'))
 router.use('/teachers', require('./teacherRoutes'))
 router.use('/courses', require('./courseRoutes'))
 router.use('/users', require('./userRoutes'))
+router.use('/students', require('./studentRoutes'))
 
 module.exports = router;
