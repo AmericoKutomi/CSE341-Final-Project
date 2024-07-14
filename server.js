0fb01cc5dbc006f109c4213042b7ea26fea06646
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2024-07-14 01:19:14 +0100

    updated

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const mongodb = require('./database/connect')
+const static = require("./routes/static")
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const port = process.env.PORT
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(static)
+app.use(express.static(__dirname + '../public'));
 app.use('/', require('./routes/index') )
 mongodb.initDb((err) => {
     if(err){
