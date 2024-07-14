23e815cecc011378d212a8e184c7a19c3619ec47
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2024-07-14 02:21:21 +0100

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 
 router.use('/', require('./swagger'))
+// routes for public file
 router.get('/', require('./static'))
-
+// routes for teachers
 router.use('/teachers', require('./teacherRoutes'))
 router.use('/courses', require('./courseRoutes'))
 router.use('/users', require('./userRoutes'))
-
+router.get('/login', passport.authenticate('github', (req, res) =>{}))
+router.get('/logout', function(req, res, next){
+    req.logOut(function(err) {
+        if(err){return next(err)}
+        res.send('You are logout, type "localhost:8080/login" to log')
+    })
+})
 module.exports = router;
