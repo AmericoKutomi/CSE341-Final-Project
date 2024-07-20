c257339cc8e1fa6289c462d7ad364a31ad15b59a
AmericoKutomi <americo@embra.com.br>
2024-07-19 23:49:04 -0300

    coursesGet.spec.js that works (incomplete)

--- __tests__/coursesGet.spec.js
+++ __tests__/coursesGet.spec.js
@@ -8,18 +8,30 @@ jest.mock('../controllers/courseController');
 jest.mock('../middlewares/authMiddleware');
 
 const app = express();
+
 app.use('/courses', courseRoutes);
 
 describe('GET /courses', () => {
   it('should call isAuthenticated middleware and courseController.getAllCourse', async () => {
+    // Mocking the isAuthenticated middleware to always call next()
     isAuthenticated.mockImplementation((req, res, next) => next());
+    
+    // Mocking the courseController.getAllCourse to send a response with 'All Courses'
     courseController.getAllCourse.mockImplementation((req, res) => res.status(200).send('All Courses'));
 
+    // Making a GET request to the /courses route
     const response = await request(app).get('/courses');
 
+    // Checking if the isAuthenticated middleware was called
     expect(isAuthenticated).toHaveBeenCalled();
+    
+    // Checking if the courseController.getAllCourse was called
     expect(courseController.getAllCourse).toHaveBeenCalled();
+    
+    // Checking if the response status is 200
     expect(response.status).toBe(200);
+    
+    // Checking if the response text is 'All Courses'
     expect(response.text).toBe('All Courses');
   });
 });
